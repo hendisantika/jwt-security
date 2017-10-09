e8d1f3766e19bc00732f24a0de914f2c9d9ffef1
Hendi Santika <hendisantika@gmail.com>
2017-10-09 22:11:33 +0700

    #13 - Added Hello Controler

--- src/main/java/com/hendisantika/security/jwtsecurity/controller/HelloController.java
+++ src/main/java/com/hendisantika/security/jwtsecurity/controller/HelloController.java
@@ -1,9 +1,19 @@
-package com.techprimers.security.jwtsecurity.controller;
-
+package com.hendisantika.security.jwtsecurity.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : jwt-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/10/17
+ * Time: 10.03
+ * To change this template use File | Settings | File Templates.
+ */
 
 @RestController
 @RequestMapping("/rest/hello")
