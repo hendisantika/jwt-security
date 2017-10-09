8ba6683b0f8d62353a9b559503d65fddf143857a
Hendi Santika <hendisantika@gmail.com>
2017-10-09 22:11:50 +0700

    #14 - Added Token Controller

--- src/main/java/com/hendisantika/security/jwtsecurity/controller/TokenController.java
+++ src/main/java/com/hendisantika/security/jwtsecurity/controller/TokenController.java
@@ -1,12 +1,21 @@
-package com.techprimers.security.jwtsecurity.controller;
+package com.hendisantika.security.jwtsecurity.controller;
 
-import com.techprimers.security.jwtsecurity.model.JwtUser;
-import com.techprimers.security.jwtsecurity.security.JwtGenerator;
+import com.hendisantika.security.jwtsecurity.security.JwtGenerator;
+import com.hendisantika.security.jwtsecurity.model.JwtUser;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * Created by IntelliJ IDEA.
+ * Project : jwt-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/10/17
+ * Time: 10.07
+ * To change this template use File | Settings | File Templates.
+ */
 @RestController
 @RequestMapping("/token")
 public class TokenController {
