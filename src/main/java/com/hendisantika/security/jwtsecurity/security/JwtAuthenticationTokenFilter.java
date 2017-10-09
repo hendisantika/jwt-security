182ba30dbebc749a9bbffed5854d6c9b453fe828
Hendi Santika <hendisantika@gmail.com>
2017-10-09 21:57:52 +0700

    #4 - Added JWT Authentiation Token Filter

--- src/main/java/com/hendisantika/security/jwtsecurity/security/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/hendisantika/security/jwtsecurity/security/JwtAuthenticationTokenFilter.java
@@ -1,6 +1,6 @@
-package com.techprimers.security.jwtsecurity.security;
+package com.hendisantika.security.jwtsecurity.security;
 
-import com.techprimers.security.jwtsecurity.model.JwtAuthenticationToken;
+import com.hendisantika.security.jwtsecurity.model.JwtAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
@@ -10,7 +10,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+/**
+ * Created by IntelliJ IDEA.
+ * Project : jwt-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/10/17
+ * Time: 09.27
+ * To change this template use File | Settings | File Templates.
+ */
 public class JwtAuthenticationTokenFilter extends AbstractAuthenticationProcessingFilter {
 
     public JwtAuthenticationTokenFilter() {
