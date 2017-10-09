3a3f617de280cfff15fb04d944349f9c93946bc4
Hendi Santika <hendisantika@gmail.com>
2017-10-09 22:00:37 +0700

    #8 - Added Main App

--- src/main/java/com/hendisantika/security/jwtsecurity/JwtSecurityApplication.java
+++ src/main/java/com/hendisantika/security/jwtsecurity/JwtSecurityApplication.java
@@ -1,7 +1,17 @@
-package com.techprimers.security.jwtsecurity;
+package com.hendisantika.security.jwtsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : jwt-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/10/17
+ * Time: 09.18
+ * To change this template use File | Settings | File Templates.
+ */
 
 @SpringBootApplication
 public class JwtSecurityApplication {
